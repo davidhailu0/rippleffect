9c8f6dccb2decdf31f97714f2c92430db5b405be
Dawit Hailu <davidhailu0@gmail.com>
2024-09-25 20:14:54 +0300

    fixed the Register Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,4 +1,5 @@
+import { MouseEventHandler } from "react";
 
-export default function Button({ title, type }: { title: string, type: string }) {
-    return <input type={type} className="bg-gradient-to-r from-[#18455F] via-[#4B7A87] to-black h-12 rounded-[50px] text-white shadow-xl font-bold mt-6 col-span-2 w-[260px] mx-auto text-center" value={title} />
+export default function Button({ title, type, callBack }: { title: string, type: "submit" | "reset" | "button", callBack: MouseEventHandler }) {
+    return <button type={type} onClick={callBack} className="bg-gradient-to-r from-[#18455F] via-[#4B7A87] to-black h-12 rounded-[50px] text-white shadow-xl font-bold mt-6 col-span-2 w-[260px] mx-auto text-center">{title} </button>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex flex-col gap-7 mt-7 items-center h-full w-[1120px] mx-auto">
       <p className="text-4xl font-bold text-white">Create your FREE account NOW!!</p>
       <p className={`${lato.className} text-[rgba(255,255,255,0.57)] text-lg font-medium leading-[23.4px] mb-9`}>Create Your Account Below and <span className="text-[#d7b398]">Get Free Access</span> to my Sales System that allows you to attract, capture and generate leads on autopilot.</p>
-      <VideoPlayer src="" />
+      <VideoPlayer src="https://storage.googleapis.com/msgsndr/C6nqv5N0ZUkTMUIxNoYx/media/6638c58eb478503bf929064b.mp4" />
       <p className={`${lato.className} text-[rgba(255,255,255,0.57)] text-base font-medium leading-[37.7px] mt-9 mb-4`}>(No previous experience or technical skills required)</p>
       <SignUpForm />
       <div className="flex gap-4 items-center">
