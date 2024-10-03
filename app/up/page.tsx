0dad0df099fc99d25a50400d00b8e51cf34b946d
Till Carlos <till@tillcarlos.com>
2024-10-03 18:32:40 +0700

    remove the up route from my side

--- app/up/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Book() {
-    return <div className="flex flex-col gap-3 mt-7 items-center w-full mx-auto">
-        <p className="text-2xl md:text-4xl font-bold text-white text-center">Healthy!</p>
-    </div>
-}
