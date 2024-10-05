8a4d5e53a1a1c1b5349958004cea3a10f51ec366
Dawit Hailu <davidhailu0@gmail.com>
2024-10-05 14:28:55 +0300

    Add Up Route

--- app/up/route.ts
+++ app/up/route.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: Request) {
-    return new Response('Ok', { status: 200 });
+        return new Response('<!DOCTYPE html><html><body style="background-color: green"></body></html>', { status: 200 });
   }
